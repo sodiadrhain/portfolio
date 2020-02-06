9fa0ff28766c21ee9c51e2b65e66e144cfec917a
sodiadrhain <adesojiawobajo@gmail.com>
2020-02-06 08:16:18 +0100

    Updates

---
+++ precache-manifest.348badd09c45853b60daa9de65cc3e39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450cac61b8bb8f5e1f0d2ecd3a727c22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c4dc37c6a07cef404a9",
+    "url": "/portfolio/static/css/main.9e7c8198.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/portfolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2c4dc37c6a07cef404a9",
+    "url": "/portfolio/static/js/main.c104299c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
